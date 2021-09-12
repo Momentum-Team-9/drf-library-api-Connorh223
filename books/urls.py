9875acddd271275c443795af840ef7125fdf4cb0
Connor Hickman <connorh223@gmail.com>
2021-09-12 16:36:39 -0400

    created serializers, got rest framework working

--- books/urls.py
+++ books/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.contrib import admin
 from rest_framework import urlpatterns
-from rest_framework.urlpatterns import format_suffix_paformat_suffix_patterns
+from rest_framework.urlpatterns import format_suffix_patterns
 from books import views
 
 urlpatterns = [
     path('books/', views.BookList.as_view(), name='book-list'),
+    path('books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
     path('', views.api_root),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- books/views.py
+++ books/views.py
@@ -1,13 +1,20 @@
+from django.db.models import query
 from django.shortcuts import render
 from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 from .models import Book
+from .serializers import BookSerializer
 # Create your views here.
 
 class BookList(generics.ListCreateAPIView):
     queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 
 
