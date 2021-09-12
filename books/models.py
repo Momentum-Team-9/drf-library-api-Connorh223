ce7e6c0a58d9b9459c58fed2b1a3d91148cace5f
Connor Hickman <connorh223@gmail.com>
2021-09-12 17:21:42 -0400

    added owner, review

--- books/models.py
+++ books/models.py
@@ -7,6 +7,8 @@ class Book(models.Model):
     author = models.CharField(max_length=250)
     genre = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
+    review = models.TextField(max_length=750)
+    owner = models.ForeignKey('auth.User', related_name='books', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
